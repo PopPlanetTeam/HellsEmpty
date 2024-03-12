736699151ba874d6b2a1a3620ec493370dcef3e4
JelsonRodrigues <jelsonrodrigues25@gmail.com>
2024-03-12 19:10:19 -0300

    feat: increase number of enemies when multiple are killed

--- game/game.gd
+++ game/game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var limit = 20
 var qtd_enemies = 0
-var killed_enemies = 0
+var killed_enemies : int = 0
 var viewport_size = get_viewport_rect().size
 @onready var audio = $Audio
 @onready var label = $player/Label
@@ -17,6 +17,7 @@ func _process(delta):
 		var position = Vector2().from_angle(randf_range(0, 2*PI)) * viewport_size
 		var enemy = load("res://enemy/enemy.tscn").instantiate()
 		enemy.position = position + player.position
+		enemy.connect("enemy_killed", _on_enemy_killed)
 		self.add_child(enemy)
 	label.text = "Almas coletadas = " + str(killed_enemies)
 
@@ -25,3 +26,11 @@ func _on_audio_finished():
 
 func _on_game_limits_body_exited(body:Node2D) -> void:
 	body.queue_free()
+
+func _on_enemy_killed():
+	self.qtd_enemies -= 1
+	self.killed_enemies += 1
+	
+	# Every 50 enemies killed the number of spawned enemies increase by 5
+	if killed_enemies % 50 == 0:
+		self.qtd_enemies += 5
