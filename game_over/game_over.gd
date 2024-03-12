1b86f6863afc0ec8fd463793b56b59b41fe884ef
JelsonRodrigues <jelsonrodrigues25@gmail.com>
2024-03-12 19:13:39 -0300

    chore: started game over scene

---
+++ game_over/game_over.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var almas_coletadas : int = 0
+@export var time_to_restart : = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AlmasColetadas.text = "Almas Coletadas: " + str(almas_coletadas)
+
+func _process(delta):
+	$TimeUntilRestart.text = str(time_to_restart) + "s"
+
+
+func _on_restart_timer_timeout():
+	time_to_restart -= 1
+	if time_to_restart == 0:
+		get_tree().change_scene_to_file("res://menus/main_menu/menu.tscn")
