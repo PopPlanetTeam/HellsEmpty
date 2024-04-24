f38fabfdc6ecb60d47eaafa583e0cbb0dcbefe68
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-04-24 17:44:44 -0300

    Options menu added

--- menus/main_menu/menu.gd
+++ menus/main_menu/menu.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready():
+	%OpcoesMenu.visible = false
+
 func _process(_delta):
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		var current_mode = DisplayServer.window_get_mode()
@@ -13,7 +16,7 @@ func _on_iniciar_pressed():
 	get_tree().change_scene_to_file("res://levels/safe_house/safe_house.tscn")
 
 func _on_opcoes_pressed():
-	pass # Replace with function body.
+	%OpcoesMenu.visible = true
 
 func _on_sair_pressed():
 	get_tree().quit()
