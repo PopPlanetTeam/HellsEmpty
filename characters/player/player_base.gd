6b84a7e71d3f22dab814a4e78e560ec7055b91d1
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-04-24 17:05:54 -0300

    refactor: extract player base functionality

---
+++ characters/player/player_base.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+class_name PlayerBase
+
+@export var player_sprite: Sprite2D
+@export var SPEED = 150.0
+
+func _physics_process(delta):
+	var x_direction = Input.get_axis("left", "right")
+	var y_direction = Input.get_axis("up", "down")
+	
+	if x_direction or y_direction:
+		velocity = Vector2(x_direction, y_direction).normalized() * SPEED
+	else:
+		velocity = Vector2(move_toward(velocity.x, 0, SPEED), move_toward(velocity.y, 0, SPEED))
+
+	player_sprite.animate(velocity)
+	move_and_slide()
