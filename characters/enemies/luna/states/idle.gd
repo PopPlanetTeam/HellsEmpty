d271a3c4c2448e71f7c678813a500bb4953dde1f
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-06-18 15:23:48 -0300

    feat: created idle state for luna

---
+++ characters/enemies/luna/states/idle.gd
@@ -0,0 +1,15 @@
+extends State
+
+@onready var _animation = %Animation
+
+## This function is automatically called when the state is entered.
+func Enter():
+	_animation.play("idle")
+
+## This function is automatically called when the state is exited.
+func Exit():
+	pass
+
+## This function is called every frame.
+func Update(_delta):
+	pass
