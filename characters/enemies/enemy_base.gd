44a903cb80546538e30b5cc5799aa3e1b28cff72
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-06-18 13:00:01 -0300

    refactor: improved EnemyBase

--- characters/enemies/enemy_base.gd
+++ characters/enemies/enemy_base.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 class_name EnemyBase
 
-signal enemy_killed
-
 @export var speed: float = 100.0
 @export var player: PlayerBase
-@export var enemy_animated_sprite: AnimatedSprite2D
 
 @export_group("Life and Damage")
 @export var health_component: Health
@@ -31,21 +28,8 @@ func _ready():
 	_hitbox.collision_mask = takes_damage
 
 func _process(delta):
-	# if player:
-	# 	var direction : Vector2 = (player.global_position - global_position).normalized()
-		
-	# 	# Do not go directly to the player, add some random noise to movement
-	# 	if randf() < RANDOM_MOVEMENT_RATE:
-	# 		movement_randomness = Vector2(randf_range(0.0, PI), randf_range(0.0, PI)).normalized() * 0.33
-	# 	global_position += (direction + movement_randomness).normalized() * speed * delta
-	
 	_life_bar.value = health_component.life
 
-func _on_damage_area_body_entered(body) -> void:
-	if body.is_in_group("inflict_damage"):
-		health_component.life -= body.damage
-		body.queue_free()
-		if health_component.life <= 0.0:
-			enemy_killed.emit()
-			queue_free()
-			
+# Take damage
+func _on_hit_box_area_entered(area):
+	_hitbox.take_damage(area.damage)
