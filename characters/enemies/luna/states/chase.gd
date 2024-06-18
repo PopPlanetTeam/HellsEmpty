9c6ac837499682a3aecd6c6a93999d3e0340d810
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-06-18 16:20:06 -0300

    feat: added chase state

---
+++ characters/enemies/luna/states/chase.gd
@@ -0,0 +1,23 @@
+extends State
+
+@onready var _enemy = $"../.."
+
+## This function is automatically called when the state is entered.
+func Enter():
+	get_tree().create_timer(1.0).timeout.connect(_on_timeout)
+
+## This function is automatically called when the state is exited.
+func Exit():
+	pass
+
+## This function is called every frame.
+func Update(delta):
+	pass
+
+func _on_timeout():
+	state_machine.change_to_state(self, "idle")
+
+func _physics_process(delta):
+	_enemy.velocity = Vector2(-1, 0) * _enemy.speed
+
+	_enemy.move_and_slide()
