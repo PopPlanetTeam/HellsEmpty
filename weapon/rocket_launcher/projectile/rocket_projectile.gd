b93aca6d060a44c37336d5ba5cdc43f41eec0882
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-08-02 03:02:47 -0300

    feat: added explosion effect

--- weapon/rocket_launcher/projectile/rocket_projectile.gd
+++ weapon/rocket_launcher/projectile/rocket_projectile.gd
@@ -5,6 +5,8 @@ class_name RocketProjectile
 @export var direction : Vector2 = Vector2.ZERO
 @export var damage : float = 50.0
 
+var _explosion_scene = preload("res://effects/explosion.tscn")
+
 func _ready():
 	$DamageArea.damage = damage
 
@@ -12,4 +14,9 @@ func _process(delta):
 	position += speed * direction * delta
 
 func _on_damage_area_damage_dealt():
+	var explosion = _explosion_scene.instantiate()
+
+	explosion.global_position = self.global_position
+	get_parent().add_child(explosion)
+
 	self.queue_free()
