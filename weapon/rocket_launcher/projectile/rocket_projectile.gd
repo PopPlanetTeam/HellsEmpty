cc2d2ce87f6e88466237834bbaf80f32a304eec6
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-08-20 22:13:51 -0300

    feat: added area damage to Rocket Launcher

--- weapon/rocket_launcher/projectile/rocket_projectile.gd
+++ weapon/rocket_launcher/projectile/rocket_projectile.gd
@@ -3,10 +3,22 @@ class_name RocketProjectile
 
 var _explosion_scene = preload("res://effects/explosion.tscn")
 
+@onready var _damage_area_shape = $DamageArea/CollisionShape2D
+@onready var _sprite = $Sprite2D
+
 func _on_hit():
+	self.speed = 0
+	_sprite.visible = false
+
 	var explosion = _explosion_scene.instantiate()
 
 	explosion.global_position = self.global_position
 	get_parent().add_child(explosion)
+
+	# Increase area of damage
+	var amount = 10
+	_damage_area_shape.scale = Vector2(amount, amount)
+
+	await get_tree().create_timer(1.0).timeout
 
 	self.queue_free()
