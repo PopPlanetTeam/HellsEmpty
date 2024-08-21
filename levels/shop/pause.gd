4843c802a5eb67864ff5786b4662eeb5daca4968
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2024-08-21 10:45:13 -0300

    chore: add pause label

--- levels/shop/pause.gd
+++ levels/shop/pause.gd
@@ -1,6 +1,13 @@
-extends Node
+extends Node2D
+
+@onready var paused_label = $Paused
+
+func _ready():
+	paused_label.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		var pause = get_tree().paused
 		get_tree().paused = !pause
+		paused_label.visible = !pause
+		
